e6e21ee479ee829b9a5df76c4067f621c2d37870
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Default.aspx.cs
@@ -0,0 +1,30 @@
+﻿using DevExpress.Web;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+
+    protected void ASPxGridView1_CustomErrorText(object sender, ASPxGridViewCustomErrorTextEventArgs e)
+    {
+        if (e.Exception is CallbackException)
+            e.ErrorText = e.Exception.Message;
+    }
+    protected void ASPxGridView1_RowUpdating(object sender, DevExpress.Web.Data.ASPxDataUpdatingEventArgs e)
+    {
+        throw new CallbackException("Data modifications are not allowed in the online example");
+    }
+}
+
+public class CallbackException : Exception
+{
+    public CallbackException(string message)
+        : base(message)
+    {
+    }
+}
